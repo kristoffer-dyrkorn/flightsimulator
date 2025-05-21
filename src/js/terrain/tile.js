a5f82a1f04fb5cfd38be2b109150d02d96fff8f0
Kristoffer Dyrkorn <>
2025-05-21 09:29:31 +0200

    Updates

--- src/js/terrain/tile.js
+++ src/js/terrain/tile.js
@@ -4,7 +4,11 @@ import { BasisTextureLoader } from "../graphics/BasisTextureLoader.js"
 const url = new URL(document.location)
 const urlParams = url.searchParams
 
-const TEXTURE_PATH = "texture"
+let TEXTURE_PATH = "texture"
+
+if (urlParams.has("v2")) {
+  TEXTURE_PATH = "texture-v2"
+}
 
 const SERVER = urlParams.has("local") ? "http://localhost:8000" : "https://s3-eu-west-1.amazonaws.com/kd-flightsim"
 
