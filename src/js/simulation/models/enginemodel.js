4e0e1c575d70a421594916af81cf5e09e59c5f0e
Kristoffer Dyrkorn <>
2025-06-02 19:19:27 +0200

    Fix wrong value

--- src/js/simulation/models/enginemodel.js
+++ src/js/simulation/models/enginemodel.js
@@ -6,11 +6,11 @@ export default class EngineModel {
   constructor() {
     this.idlePowerData = new DataTable2D([
       [1060, 670, 880, 1140, 1500, 1860],
-      [6350, 425, 690, 1010, 1330, 1700],
+      [635, 425, 690, 1010, 1330, 1700],
       [60, 25, 345, 755, 1130, 1525],
       [-1020, -710, -300, 350, 910, 1360],
       [-2700, -1900, -1300, -247, 600, 1100],
-      [-3600, -1400, -595, -342, -200, 700]
+      [-3600, -1400, -595, -342, -200, 700],
     ])
     this.idlePowerData.setXRange(SimulationConstants.ALTITUDE_MIN, SimulationConstants.ALTITUDE_MAX)
     this.idlePowerData.setYRange(SimulationConstants.POWER_MIN, SimulationConstants.POWER_MAX)
@@ -21,7 +21,7 @@ export default class EngineModel {
       [12610, 9312, 6610, 4290, 2600, 1560],
       [12640, 9839, 7090, 4660, 2840, 1660],
       [12390, 10176, 7750, 5320, 3250, 1930],
-      [11680, 9848, 8050, 6100, 3800, 2310]
+      [11680, 9848, 8050, 6100, 3800, 2310],
     ])
     this.milPowerData.setXRange(SimulationConstants.ALTITUDE_MIN, SimulationConstants.ALTITUDE_MAX)
     this.milPowerData.setYRange(SimulationConstants.POWER_MIN, SimulationConstants.POWER_MAX)
@@ -32,7 +32,7 @@ export default class EngineModel {
       [22700, 16860, 12250, 8154, 5000, 2835],
       [24240, 18910, 13760, 9285, 5700, 3215],
       [26070, 21075, 15975, 11115, 6860, 3950],
-      [28886, 23319, 18300, 13484, 8642, 5057]
+      [28886, 23319, 18300, 13484, 8642, 5057],
     ])
     this.maxPowerData.setXRange(SimulationConstants.ALTITUDE_MIN, SimulationConstants.ALTITUDE_MAX)
     this.maxPowerData.setYRange(SimulationConstants.POWER_MIN, SimulationConstants.POWER_MAX)
