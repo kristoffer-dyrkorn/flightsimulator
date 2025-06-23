f0d0d85858bd6d38c9431d25e7d60f310236fc48
Kristoffer Dyrkorn <>
2025-06-23 21:50:17 +0200

    Fixes

--- src/js/hifimodel/models/atmosphericmodel.js
+++ src/js/hifimodel/models/atmosphericmodel.js
@@ -9,14 +9,15 @@ export default class AtmosphericModel {
    * Computes properties of the standard atmosphere.
    */
   update(vt, alt) {
-    let tfac = 1.0 - alt * 0.703e-5
-    let t = 519.0 * tfac
-    if (alt >= 35000.0) {
-      t = 390.0
+    let tfac = 1.0 - alt / 145442
+    let t = 518.67 * tfac
+    if (alt >= 36089) {
+      t = 389.97
     }
-    let rho = 0.002377 * Math.pow(tfac, 4.14)
-    this.rmach = vt / Math.sqrt(1.4 * 1716.3 * t)
+    let rho = 0.002377 * Math.pow(tfac, 4.255876)
+
+    this.rmach = vt / Math.sqrt(1.403 * 1716.56 * t)
     this.qbar = 0.5 * rho * vt * vt
-    this.ps = 1715.0 * rho * t
+    this.ps = 1716.56 * rho * t
   }
 }
